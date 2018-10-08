739d5c035a28c104bd1e3e60f1f746c44526ed77
Vindexus <vindexus@gmail.com>
2018-10-07 19:53:08 -0700

    Now it gets a .zip of a backup from S3 and uses that, so it can select destination db

--- restore.js
+++ restore.js
@@ -5,6 +5,7 @@ const fs = require('fs'),
   path = require('path'),
   exec = require('child_process').exec,
   AWS = require('aws-sdk'),
+  AdmZip = require('adm-zip'),
   MongoDBURI = require('mongodb-uri');
 
 // Validate Configuration
@@ -12,8 +13,8 @@ function ValidateConfig(config) {
   if (config && config.mongodb && config.s3 && config.s3.accessKey && config.s3.secretKey && config.s3.region && config.s3.bucketName) {
 
     // Don't try to parse the url when it has been parsed once
-    if (!(config.mongodb.scheme && config.mongodb.hosts)) {
-      config.mongodb = MongoDBURI.parse(config.mongodb);
+    if (typeof config.mongodb == 'string') {
+      config.mongodb = MongoDBURI.parse(config.mongodb)
     }
 
     return true;
@@ -34,10 +35,10 @@ function AWSSetup(config) {
 // Fetch a list of all files from AWS
 function listObjectsInBucket(s3, config) {
   return new Promise((resolve, reject) => {
-    s3.listObjects({
-      Bucket: config.s3.bucketName,
-      Prefix: config.mongodb.database || ""
-    }, (err, data) => {
+    const opts = {
+      Bucket: config.s3.bucketName
+    }
+    s3.listObjects(opts, (err, data) => {
       if (err) {
         reject(err)
       } else {
@@ -59,22 +60,40 @@ function restore(config, filePath) {
       port = config.mongodb.hosts[0].port || null;
 
     let gzipFlag = "--gzip"
+    let dropFlag = ""
 
     if (path.extname(filePath).toLowerCase() != ".gz") {
       gzipFlag = ""
     }
 
+    if (config.drop) {
+      dropFlag = ' --drop'
+    }
+
+    const parts = filePath.split(/[\/\\]+/) //Split by slashes
+    const file = parts[parts.length - 1]
+    const originalDb = file.split('_')[0]
+
+    console.log('originalDb', originalDb)
+
+    const separator = filePath.indexOf('/') > -1 ? '/' : '\\'
+
+    filePath += separator + originalDb
+    console.log('filePath', filePath);
+
     // Default command, does not considers username or password
-    let command = `mongorestore -h ${host} --port=${port} -d ${database} ${gzipFlag} --archive=${filePath}`;
+    let command = `mongorestore -h ${host} --port=${port} -d ${database} ${gzipFlag} ${dropFlag} ${filePath}`;
 
     // When Username and password is provided
     if (username && password) {
-      command = `mongorestore -h ${host} --port=${port} -d ${database} -p ${password} -u ${username} ${gzipFlag} --archive=${filePath}`;
+      command = `mongorestore -h ${host} --port=${port} -d ${database} -p ${password} -u ${username} ${gzipFlag} ${dropFlag} ${filePath}`;
     }
     // When Username is provided
     if (username && !password) {
-      command = `mongorestore -h ${host} --port=${port} -d ${database} -u ${username} ${gzipFlag} --archive=${filePath}`;
+      command = `mongorestore -h ${host} --port=${port} -d ${database} -u ${username} ${gzipFlag} ${dropFlag} ${filePath}`;
     }
+
+    console.log('command', command)
 
     exec(command, {maxBuffer: 1024 * 500}, (err, stdout, stderr) => {
       if (err) {
@@ -92,31 +111,73 @@ function restore(config, filePath) {
 
 }
 
+function downloadAndUnzipBackup (s3, config, backupKey) {
+  console.log(`downloading then unzip`)
+  return new Promise((resolve, reject) => {
+    console.log(`downloading`)
+    return downloadBackup(s3, config, backupKey)
+      .then(result => {
+        console.log('dl result', result)
+        const zip = new AdmZip(result.filePath)
+        const extractTo = result.filePath.replace('.zip', '')
+        console.log('extractTo', extractTo)
+        zip.extractAllTo(extractTo)
+        result.filePath = extractTo
+        console.log('126result', result)
+        resolve(result)
+      })
+  })
+}
+
 // Download Backup
 function downloadBackup(s3, config, backupKey) {
+  console.log('backupKey', backupKey);
+
+  const obj = {
+    Bucket: config.s3.bucketName,
+    Key: backupKey
+  }
+
+  console.log('obj', obj)
 
   return new Promise((resolve, reject) => {
-    s3.getObject({
-      Bucket: config.s3.bucketName,
-      Key: backupKey
-    }, (err, data) => {
+    console.log(`downloading`)
+    s3.getObject(obj, (err, data) => {
+      console.log('get object err', err);
       if (err) {
         reject({error: 1, code: err.code, message: err.message})
       } else {
         let stream = fs.createWriteStream(path.resolve(os.tmpdir(), backupKey));
 
-        stream.write(data.Body);
+        stream.on('open', err => {
+          console.log(`write some data`)
+          stream.on('finish', () => {
+            console.log(`finish called`)
+            resolve({
+              error: 0,
+              filePath: path.resolve(os.tmpdir(), backupKey)
+            })
+          })
+
+          stream.on('error', err => {
+            console.log('err', err)
+            reject({error: 1, message: err.message, code: err.code})
+            return
+          })
+
+          stream.write(data.Body)
+          stream.end()
+        })
+
 
         // Reject in case of an error in writing data
-        stream.on('error', err => {
-          reject({error: 1, message: err.message, code: err.code})
-          return
-        });
+        
+        console.log('Object.keys(data)', Object.keys(data))
+        console.log('data.Body.length', data.Body.length)
+        //stream.end()
+        setTimeout(() => {
 
-        resolve({
-          error: 0,
-          filePath: path.resolve(os.tmpdir(), backupKey)
-        });
+        }, 0)
       };
     });
   })
@@ -124,18 +185,21 @@ function downloadBackup(s3, config, backupKey) {
 
 // Validate, Download, Restore Backup
 function RestoreBackup(config, databaseToRestore) {
-  let isValidConfig = ValidateConfig(config);
+  console.log('databaseToRestore', databaseToRestore);
+  console.log('config', config);
+  let isValidConfig = ValidateConfig(config)
 
   if (isValidConfig) {
-
     let s3 = AWSSetup(config)
-    return downloadBackup(s3, config, databaseToRestore).then(result => {
+    return downloadAndUnzipBackup(s3, config, databaseToRestore).then(result => {
+      console.log('result', result)
       return restore(config, result.filePath)
     }, error => {
       return Promise.reject({error: 1, message: error.message});
     })
 
   } else {
+    console.log(`not a valid confirguration`)
     return Promise.reject({error: 1, message: "Invalid Configuration"})
   }
 }
